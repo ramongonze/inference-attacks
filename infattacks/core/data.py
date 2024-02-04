0c8aa10c44ed9ce56d17153cf1823f07d6905d67
Ramon G. Gonze <ramongonze@gmail.com>
2024-02-04 21:34:05 +0100

    Add option to create a dataset instance from a pandas.DataFrame.

--- infattacks/core/data.py
+++ infattacks/core/data.py
@@ -20,12 +20,13 @@ class Data:
         - columns (list): The list of column names in the dataset.
     """
 
-    def __init__(self, file_path: str, file_type="csv", sep=None, r_dataframe_name=None, na_values=-1) -> None:
+    def __init__(self, dataframe=None, file_path=None, file_type="csv", sep=None, r_dataframe_name=None, na_values=-1):
         """
         Initializes a Dataset object.
 
         Args:
-            file_path (str): The path to the dataset file.
+            dataframe (pandas.DataFrame, optional): A pandas DataFrame containing the dataset.
+            file_path (str, optional): The path to the dataset file.
             file_type (str, optional): The type of the dataset file, default is "csv".
             sep (str, optional): The delimiter used in CSV files. Required if file_type is "csv".
             r_dataframe_name (str, optional): The name of the R DataFrame if file_type is "RData".
@@ -34,19 +35,23 @@ class Data:
         Raises:
             NameError: If required parameters are not declared.
         """
-        if file_type == "csv":
+        if dataframe is not None:
+            if isinstance(dataframe, pd.DataFrame):
+                self.dataframe = dataframe
+            else:
+                raise TypeError("dataframe must be a pandas.DataFrame object")
+        elif file_type == "csv":
             if sep is None:
                 raise NameError("sep must be provided for CSV files.")
     
             self.dataframe = pd.read_csv(file_path, sep=sep)
-            self.dataframe.replace(np.nan, na_values, inplace=True)
         elif file_type == "RData":
             if r_dataframe_name is None:
                 raise NameError("r_dataframe_name must be provided for RData files.")
             
             self.dataframe = pyreadr.read_r(file_path)[r_dataframe_name]
-            self.dataframe.replace(np.nan, na_values, inplace=True)
         
+        self.dataframe.replace(np.nan, na_values, inplace=True)
         self.num_rows = self.dataframe.shape[0]
         self.num_cols = self.dataframe.shape[1]
         self.columns = self.dataframe.columns.to_list()
